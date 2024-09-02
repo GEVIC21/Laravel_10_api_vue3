3a1d215275baad094e6f44241de69d164c981fa9
GEVIC21 <ndilbedouvic@gmail.com>
2024-09-02 18:35:11 +0000

    Api v1, v2

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -10,4 +10,9 @@ class Task extends Model
     use HasFactory;
 
     protected $fillable = ['name','is_completed'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
